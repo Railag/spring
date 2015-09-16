bcc407e19d1805f96dfad468c771107a42fccdf9
Railag <whenthegroundcavedin@gmail.com>
2015-09-16 14:38:25 +0300

    setup progress

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -1,12 +1,20 @@
 package demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+	@RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -17,4 +25,6 @@ public class DemoApplication {
 
          obj.getMessage();
     }
+   
+        
 }
