93caf61d698258a3063a3a133597ecde5a17e136
Railag <whenthegroundcavedin@gmail.com>
2015-11-10 14:32:24 +0300

    some controller refactoring

--- src/main/java/com/firrael/spring/controllers/HomeController.java
+++ src/main/java/com/firrael/spring/controllers/HomeController.java
@@ -19,7 +19,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -47,9 +46,6 @@ import com.firrael.spring.parsing.HabrHandler;
 import com.firrael.spring.security.Role;
 import com.firrael.spring.utils.Utf8Serializer;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 
@@ -57,171 +53,13 @@ public class HomeController {
 
 	private List<Article> articles = new ArrayList<>();
 	
-	@Autowired
-	@Qualifier("passwordEncoder")
-	private PasswordEncoder encoder;
-
 	private static Logger logger = Logger.getLogger(HomeController.class.getName());
 
 	@Autowired
 	@Qualifier("redisTemplate")
 	private RedisTemplate<String, String> template;
 
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login(Locale locale, Model model) {
-		return "login";
-	}
-
-	@RequestMapping(value = "/admin", method = RequestMethod.GET)
-	public String admin(Locale locale, Model model) {
-		return "admin";
-	}
-
-	@RequestMapping(value = "/register", method = RequestMethod.GET)
-	public String register(Locale locale, Model model) {
-		User user = new User();
-		/*
-		 * user.setEmail("test"); user.setAuthToken("testToken");
-		 * user.setLogin("user"); user.setPassword("test password");
-		 */
-
-		model.addAttribute("user", user);
-
-		return "register";
-	}
-
-	@RequestMapping(value = "/saveUser", method = RequestMethod.POST)
-	public String saveUser(Locale locale, Model model, Principal principal, @ModelAttribute("user") User user) {
-
-		user.setLoggedIn(true);
-
-		Utf8Serializer serializer = new Utf8Serializer();
-
-		user.setLogin(serializer.deserialize(user.getLogin()));
-		
-		user.setPassword(encoder.encode(user.getPassword()));
-				
-		user.setRole(Role.USER);
-
-		List<String> favArticleHashes = new ArrayList<>();
-		user.setFavoriteArticleHashes(favArticleHashes);
-
-		List<String> selectedCategories = Redis.getAllCids();
-		user.setSelectedCategories(selectedCategories);
-
-		List<String> selectedChannels = Redis.getAllChids();
-		user.setSelectedChannels(selectedChannels);
-
-		Redis.saveUser(user);
-
-		return home(locale, model, principal, 0);
-	}
-
-	@RequestMapping(value = "/selection", method = RequestMethod.GET)
-	public String selection(Locale locale, Model model, Principal principal) {
-
-		String login = principal.getName();
-
-		UserStorage storage = new UserStorage();
-		User user = storage.findUserByLogin(login);
-
-		List<Channel> userChannels = Redis.getChannelsForUser(user);
-		List<Category> userCategories = Redis.getCategoriesForUser(user);
-
-		List<Channel> allChannels = Redis.getAllChannels();
-		List<Category> allCategories = Redis.getAllCategories();
-		
-		Collections.sort(userChannels);
-		Collections.sort(userCategories);
-		Collections.sort(allChannels);
-		Collections.sort(allCategories);
-
-		SelectionModel selectionModel = new SelectionModel();
-		selectionModel.setAllCategories(allCategories);
-		selectionModel.setAllChannels(allChannels);
-		selectionModel.setSelectedCategories(userCategories);
-		selectionModel.setSelectedChannels(userChannels);
-
-		model.addAttribute("selectionModel", selectionModel);
-
-		return "selection";
-	}
-
-	@RequestMapping(value = { "/selection" }, method = RequestMethod.POST, params = "selectedChannels")
-	public String selectionChannels(Locale locale, Model model, Principal principal,
-			@RequestParam List<Channel> selectedChannels) {
-		String login = principal.getName();
-
-		UserStorage storage = new UserStorage();
-		User user = storage.findUserByLogin(login);
-
-		Redis.updateUserChannels(user, selectedChannels);
-
-		return selection(locale, model, principal);
-	}
-
-	@RequestMapping(value = { "/selection" }, method = RequestMethod.POST, params = "selectedCategories")
-	public String selectionCategories(Locale locale, Model model, Principal principal,
-			@RequestParam List<Category> selectedCategories) {
-		String login = principal.getName();
-
-		Utf8Serializer serializer = new Utf8Serializer();
-
-		for (Category c : selectedCategories) {
-			c.setName(serializer.deserialize(c.getName()));
-		}
-
-		UserStorage storage = new UserStorage();
-		User user = storage.findUserByLogin(login);
-
-		Redis.updateUserCategories(user, selectedCategories);
-
-		return selection(locale, model, principal);
-	}
-
-	@RequestMapping(value = "/users", method = RequestMethod.GET)
-	public String users(Locale locale, Model model, Principal principal, @RequestParam(required = false) Integer page) {
-
-		// only for admin
-		String login = principal.getName();
-
-		UserStorage storage = new UserStorage();
-		List<User> users = storage.getItems(100);
-
-		List<UserPage> pages = (List<UserPage>) PageCreator.getPagingList(users, UserPage.class);
-
-		model.addAttribute("pages", pages);
-
-		if (page == null || page >= pages.size() || page < 0)
-			page = 0;
-
-		model.addAttribute("currentPage", pages.get(page));
-
-		return "adminUsers";
-	}
-
-	@RequestMapping(value = "/articles", method = RequestMethod.GET)
-	public String articles(Locale locale, Model model, Principal principal,
-			@RequestParam(required = false) Integer page) {
-
-		// only for admin
-		String login = principal.getName();
-
-		ArticleStorage storage = new ArticleStorage();
-		List<Article> articles = storage.getItems(100);
-
-		List<ArticlePage> pages = (List<ArticlePage>) PageCreator.getPagingList(articles, ArticlePage.class);
-
-		model.addAttribute("pages", pages);
-
-		if (page == null || page >= pages.size() || page < 0)
-			page = 0;
-
-		model.addAttribute("currentPage", pages.get(page));
-
-		return "adminArticles";
-	}
-
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
