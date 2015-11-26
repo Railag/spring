387ffcb7c2a3fddfd365b2ec44a544f18c84797b
Railag <whenthegroundcavedin@gmail.com>
2015-11-26 13:23:30 +0300

    ajax fav request small fix

--- src/main/java/com/firrael/spring/data/models/SimpleResponse.java
+++ src/main/java/com/firrael/spring/data/models/SimpleResponse.java
@@ -1,8 +1,13 @@
 package com.firrael.spring.data.models;
 
+import com.fasterxml.jackson.annotation.JsonRawValue;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public class SimpleResponse {
 	private String responseJson;
 
+	@JsonValue
+    @JsonRawValue
 	public String getResponseJson() {
 		return responseJson;
 	}
